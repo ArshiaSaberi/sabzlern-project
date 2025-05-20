a51d75e50d2e11508aee4a119292b1fe4da1e1dc
arshia <arshiasaberi015@gmail.com>
2025-05-20 22:25:46 +0330

    stat

---
+++ js.js
@@ -0,0 +1,35 @@
+let navbtn = document.querySelector(".nav_btn");
+let foterbtn = document.querySelector(".foter_btn")
+let ww = document.querySelector("#ww")
+let menumobils = document.querySelector(".menu_mobils");
+navbtn.addEventListener("click", () => {
+    navbtn.classList.toggle("nav_btn--open");
+    menumobils.classList.toggle("menu_mobils--open");
+    ww.classList.add("wr")
+
+})
+
+
+window.addEventListener("click", (e) => {
+    if (e.target == ww) {
+
+
+        menumobils.classList.remove("menu_mobils--open");
+        ww.classList.remove("wr")
+        navbtn.classList.remove("nav_btn--open");
+    }
+})
+foterbtn.addEventListener("click", () => {
+    window.scrollTo(0, 0)
+})
+
+let like = document.querySelectorAll("svg")
+like.forEach((e) => {
+    e.addEventListener("click", (e) => {
+        e.target.classList.toggle("like__op")
+    })
+
+
+})
+
+
